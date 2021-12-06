89b78d7a6983f1c655b45efb4146d4ec2ccef8d1
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2021-12-06 21:48:33 +0300

    Добавьте файлы проекта.

---
+++ Struct_11_2_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_11_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearEquation linEq = new LinearEquation { k = 2, b = -1 };
+            linEq.Root();
+            Console.ReadKey();
+        }
+    }
+    struct LinearEquation
+    {
+        public double k;
+        public double b;
+
+        public void Root()
+        {
+            double x = -b / k;
+            Console.WriteLine(x);
+        }
+    }
+}
